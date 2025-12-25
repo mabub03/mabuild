48a1a32c581e76204f0ba10250e8957d3a2bb9ba
mabub03 <mabub03@gmail.com>
2025-12-25 02:42:02 -0500

    fixed script and added lazygit

--- files/scripts/font_rendering_symlinks.sh
+++ files/scripts/font_rendering_symlinks.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 set -oue pipefail
 
-ln -s /usr/share/fontconfig/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d
-ln -s /usr/share/fontconfig/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d
+CONF_DIR="/etc/fonts/conf.d"
+
+# if the symlink below isn't there then symlink from /usr/share
+if [ ! -L "$CONF_DIR/11-lcdfilter-default.conf" ]; then
+  ln -s /usr/share/fontconfig/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d
+fi
+
+if [ ! -L "$CONF_DIR/10-sub-pixel-rgb.conf" ]; then
+  ln -s /usr/share/fontconfig/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d
+fi
