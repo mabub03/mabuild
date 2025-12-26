e3bd63c4b16ab4ffffbeea73f6467cc1686d797b
mabub03 <mabub03@gmail.com>
2025-12-25 19:07:07 -0500

    actually added browser policies and firefox config

---
+++ files/system/etc/firefox/pref/user.js
@@ -0,0 +1,78 @@
+user_pref("browser.startup.homepage", "about:home");
+user_pref("browser.uidensity", 1);
+user_pref("browser.tabs.groups.enabled", true);
+user_pref("sidebar.revamp.round-content-area", true);
+user_pref("sidebar.revamp", true);
+user_pref("sidebar.main.tools", "aichat,syncedtabs,history,bookmarks");
+user_pref("sidebar.visibility", "expand-on-hover");
+user_pref("sidebar.verticalTabs", true);
+user_pref("browser.download.autohideButton", false);
+user_pref("browser.download.panel.shown", true);
+user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
+user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
+user_pref(
+  "browser.newtabpage.activity-stream.newtabWallpapers.highlightDismissed",
+  true,
+);
+user_pref(
+  "browser.newtabpage.activity-stream.newtabWallpapers.wallpaper-dark",
+  "dark-mountain",
+);
+user_pref(
+  "browser.newtabpage.activity-stream.newtabWallpapers.wallpaper-light",
+  "light-mountain",
+);
+user_pref("browser.newtabpage.activity-stream.showWeather", false);
+user_pref("browser.bookmarks.restore_default_bookmarks", false);
+user_pref("browser.compactmode.show", true);
+user_pref("browser.contentblocking.category", "strict");
+user_pref("browser.toolbarbuttons.introduced.sidebar-button", true);
+user_pref("browser.toolbars.bookmarks.visibility", "never");
+user_pref("font.default.x-western", "sans-serif");
+user_pref("font.name.monospace.ja", "Noto Sans Mono CJK JP");
+user_pref("font.name.monospace.ko", "Noto Sans Mono CJK KR");
+user_pref("font.name.monospace.zh-CN", "Noto Sans Mono CJK SC");
+user_pref("font.name.monospace.zh-HK", "Noto Sans Mono CJK HK");
+user_pref("font.name.monospace.zh-TW", "Noto Sans Mono CJK TC");
+user_pref("font.name.sans-serif.ja", "IBM Plex Sans JP");
+user_pref("font.name.sans-serif.ko", "IBM Plex Sans KR");
+user_pref("font.name.sans-serif.zh-CN", "IBM Plex Sans SC");
+user_pref("font.name.sans-serif.zh-HK", "IBM Plex Sans TC");
+user_pref("font.name.sans-serif.zh-TW", "IBM Plex Sans TC");
+user_pref("font.name.serif.ja", "Noto Serif CJK JP");
+user_pref("font.name.serif.ko", "Noto Serif CJK KR");
+user_pref("font.name.serif.zh-CN", "Noto Serif CJK SC");
+user_pref("font.name.serif.zh-HK", "Noto Serif CJK HK");
+user_pref("font.name.serif.zh-TW", "Noto Serif CJK TC");
+user_pref("font.cjk_pref_fallback_order", "ja,zh-cn,zh-hk,zh-tw,ko");
+user_pref("privacy.clearOnShutdown.cookies", false);
+user_pref("privacy.clearOnShutdown.downloads", false);
+user_pref("privacy.clearOnShutdown.formdata", false);
+user_pref("privacy.clearOnShutdown.history", false);
+user_pref("privacy.clearOnShutdown.sessions", false);
+user_pref("privacy.clearOnShutdown_v2.browsingHistoryAndDownloads", false);
+user_pref("privacy.clearOnShutdown_v2.cookiesAndStorage", false);
+user_pref("privacy.clearOnShutdown_v2.historyFormDataAndDownloads", false);
+user_pref("privacy.donottrackheader.enabled", true);
+user_pref("privacy.fingerprintingProtection", true);
+user_pref("privacy.trackingprotection.emailtracking.enabled", true);
+user_pref("privacy.trackingprotection.enabled", true);
+user_pref("privacy.trackingprotection.socialtracking.enabled", true);
+user_pref("widget.gtk.libadwaita-colors.enabled", false);
+user_pref("widget.gtk.rounded-bottom-corners.enabled", true);
+user_pref(
+  "browser.uiCustomization.horizontalTabsBackup",
+  '{"placements":{"widget-overflow-fixed-list":[],"unified-extensions-area":["addon_darkreader_org-browser-action","_762f9885-5a13-4abd-9c77-433dcd38b8fd_-browser-action","frankerfacez_frankerfacez_com-browser-action"],"nav-bar":["sidebar-button","back-button","forward-button","stop-reload-button","customizableui-special-spring1","vertical-spacer","urlbar-container","customizableui-special-spring2","_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action","ublock0_raymondhill_net-browser-action","unified-extensions-button","downloads-button","fxa-toolbar-menu-button"],"toolbar-menubar":["menubar-items"],"TabsToolbar":["tabbrowser-tabs","new-tab-button","alltabs-button"],"vertical-tabs":[],"PersonalToolbar":["personal-bookmarks"]},"seen":["save-to-pocket-button","developer-button","_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action","addon_darkreader_org-browser-action","_762f9885-5a13-4abd-9c77-433dcd38b8fd_-browser-action","ublock0_raymondhill_net-browser-action","frankerfacez_frankerfacez_com-browser-action","screenshot-button"],"dirtyAreaCache":["nav-bar","vertical-tabs","toolbar-menubar","TabsToolbar","PersonalToolbar","unified-extensions-area"],"currentVersion":23,"newElementCount":7}',
+);
+user_pref(
+  "browser.uiCustomization.horizontalTabstrip",
+  '["tabbrowser-tabs","new-tab-button","alltabs-button"]',
+);
+user_pref(
+  "browser.uiCustomization.navBarWhenVerticalTabs",
+  '["sidebar-button","back-button","forward-button","stop-reload-button","customizableui-special-spring1","vertical-spacer","urlbar-container","customizableui-special-spring2","_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action","ublock0_raymondhill_net-browser-action","unified-extensions-button","downloads-button","fxa-toolbar-menu-button","new-tab-button","alltabs-button"]',
+);
+user_pref(
+  "browser.uiCustomization.state",
+  '{"placements":{"widget-overflow-fixed-list":[],"unified-extensions-area":["addon_darkreader_org-browser-action","_762f9885-5a13-4abd-9c77-433dcd38b8fd_-browser-action","frankerfacez_frankerfacez_com-browser-action"],"nav-bar":["sidebar-button","back-button","forward-button","stop-reload-button","customizableui-special-spring1","vertical-spacer","urlbar-container","customizableui-special-spring2","_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action","ublock0_raymondhill_net-browser-action","unified-extensions-button","downloads-button","fxa-toolbar-menu-button","new-tab-button","alltabs-button"],"toolbar-menubar":["menubar-items"],"TabsToolbar":[],"vertical-tabs":["tabbrowser-tabs"],"PersonalToolbar":["personal-bookmarks"]},"seen":["save-to-pocket-button","developer-button","_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action","addon_darkreader_org-browser-action","_762f9885-5a13-4abd-9c77-433dcd38b8fd_-browser-action","ublock0_raymondhill_net-browser-action","frankerfacez_frankerfacez_com-browser-action","screenshot-button"],"dirtyAreaCache":["nav-bar","vertical-tabs","toolbar-menubar","TabsToolbar","PersonalToolbar","unified-extensions-area"],"currentVersion":23,"newElementCount":7}',
+);
