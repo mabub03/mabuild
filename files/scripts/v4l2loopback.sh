09c3bbc17c6d25a7e5948968a9d38e99a4d45fb2
mabub03 <mabub03@gmail.com>
2025-12-27 02:02:48 -0500

    Update v4l2loopback.sh

--- files/scripts/v4l2loopback.sh
+++ files/scripts/v4l2loopback.sh
@@ -2,6 +2,9 @@ dnf -y config-manager addrepo --from-repofile=https://raw.githubusercontent.com/
 
 sudo dnf in -y v4l2loopback
 
-VER=$(ls /lib/modules) && akmods --force --kernels $VER --kmod v4l2loopback
+VER=$(ls /lib/modules) &&
+  akmods --force --kernels $VER --kmod v4l2loopback &&
+  depmod -a $VER &&
+  dracut --kver $VER --force --add ostree --no-hostonly --reproducible /usr/lib/modules/$VER/initramfs.img
 
 rm -f /etc/yum.repos.d/{*terra*}.repo
